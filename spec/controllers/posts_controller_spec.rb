310877e749d207fb5abfaf3ba6917c0ac11cf108
Christopher Roper <chrisjmroper@gmail.com>
2017-07-03 14:54:36 -0600

    Add RSpec for post

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+ let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_post] to @posts" do
+      get :index
+# #9
+      expect(assigns(:posts)).to eq([my_post])
     end
   end
 
